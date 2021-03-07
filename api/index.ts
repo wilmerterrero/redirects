ef9bdfd0a1fd72377f9d6f2a93090ff8c608c25f
goncy <gonzalo.pozzo4@gmail.com>
2021-03-07 15:56:09 -0300

    Use destructuring

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { NowRequest, NowResponse } from '@vercel/node'
 import redirections from "../redirections.json"
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const subdomain = (req.headers['x-forwarded-host'] as string).split(`.`)[0];
+  const [subdomain] = (req.headers['x-forwarded-host'] as string).split(`.`);
   const redirection = redirections[subdomain] || process.env.FALLBACK;
 
   return res.writeHead(302, {Location: redirection}).end();
